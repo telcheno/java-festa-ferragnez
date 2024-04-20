d2390d4263c182bca6da958c42f6f21a93c7aed6
Telmo Checa <telmocheca1@gmail.com>
2024-04-20 22:19:56 +0200

    escludo .classpath .project

---
+++ src/main/java/it/checatel/FestaFerragnez.java
@@ -0,0 +1,10 @@
+package it.checatel;
+
+public class FestaFerragnez {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Venbenuti alla festa");
+	}
+
+}
